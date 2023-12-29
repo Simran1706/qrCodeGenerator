78a43bd3dc0a0c36a60417b68c1689d9ac668c99
simran gupta <127384464+Simran1706@users.noreply.github.com>
2023-12-29 16:24:18 +0530

    Add files via upload

---
+++ QRCodeGenerate/src/main/java/GenerateQRCode.java
@@ -0,0 +1,20 @@
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+public class GenerateQRCode {
+    public static void main(String[] args) throws WriterException, IOException {
+        String data ="https://play.google.com/store/apps/details?id=com.shopindia&pli=1";
+        String path="C:\\Users\\simran\\Desktop/QrCode.jpg";
+
+        BitMatrix matrix = new MultiFormatWriter().encode(data, BarcodeFormat.QR_CODE,500,500);
+        MatrixToImageWriter.writeToPath(matrix,"jpg", Paths.get(path));
+        System.out.println("QR code Successfully generate ");
+    }
+
+}
